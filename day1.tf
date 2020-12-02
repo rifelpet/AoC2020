6d90044286a50fa19842982913354cc28b8e2d95
Peter Rifel <pgrifel@gmail.com>
2020-12-01 22:11:40 -0600

    make terraform outputs contain the day since they are globally scoped

--- day1.tf
+++ day1.tf
@@ -2,10 +2,10 @@ locals {
   nums = [for s in compact(split("\n", file("${path.module}/day1.input"))) : tonumber(s)]
 }
 
-output "part1" {
+output "day1-part1" {
   value = compact([for i in local.nums : contains(local.nums, 2020 - i) ? i * (2020 - i) : ""])[0]
 }
-output "part2" {
+output "day1-part2" {
   value = compact(
     flatten(
       [for i in local.nums :
