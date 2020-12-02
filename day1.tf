d9813dbf7bf16862c9b13364e8a2f1498245640b
Peter Rifel <pgrifel@gmail.com>
2020-12-01 22:02:01 -0600

    Add solutions for day 1 (parts 1 and 2)
    ```
    $ terraform apply                                                                                          ✔  2m 56s  09:54:26 PM
    
    Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
    
    Outputs:
    
    part1 = ...
    part2 = ...
    ```

---
+++ day1.tf
@@ -0,0 +1,18 @@
+locals {
+  nums = [for s in compact(split("\n", file("${path.module}/day1.input"))) : tonumber(s)]
+}
+
+output "part1" {
+  value = compact([for i in local.nums : contains(local.nums, 2020 - i) ? i * (2020 - i) : ""])[0]
+}
+output "part2" {
+  value = compact(
+    flatten(
+      [for i in local.nums :
+        [for j in slice(local.nums, index(local.nums, i), length(local.nums)) :
+          contains(local.nums, 2020 - i - j) ? i * j * (2020 - i - j) : ""
+        ]
+      ]
+    )
+  )[0]
+}
